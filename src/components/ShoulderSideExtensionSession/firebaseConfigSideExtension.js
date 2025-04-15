3a659667087f526bdf6c8c502048bace241cedbf
Pranav Deshpande <ajinkyapranav4@gmail.com>
2025-04-15 08:14:58 +0530

    Updated Firebase config for Side Extension session

--- src/components/ShoulderSideExtensionSession/firebaseConfigSideExtension.js
+++ src/components/ShoulderSideExtensionSession/firebaseConfigSideExtension.js
@@ -2,25 +2,20 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: "your-api-key",
-//   authDomain: "your-auth-domain",
-//   databaseURL: "your-database-url", // Important for Realtime DB
-//   projectId: "your-project-id",
-//   storageBucket: "your-storage-bucket",
-//   messagingSenderId: "your-sender-id",
-//   appId: "your-app-id",
-// };
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrAqYWaqE7dQp6CjSBnMKTCVYWtClYtqY",
-  authDomain: "physiofyp-7d9cd.firebaseapp.com",
-  projectId: "physiofyp-7d9cd",
-  storageBucket: "physiofyp-7d9cd.appspot.com",
-  messagingSenderId: "728895826479",
-  appId: "1:728895826479:web:2ebce4fb42ac81619ef4da",
-  measurementId: "G-TZED65MPLF",
-  databaseURL: "https://physiofyp-7d9cd-default-rtdb.asia-southeast1.firebasedatabase.app"
+ 
+
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 };
 
 
